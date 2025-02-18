1b47f59ff6b743dceb9763e3b50cbe1c8046b6d2
linih <nightsky257@gmail.com>
2025-02-19 00:56:57 +0800

    no message

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Cal from './Cal';
+import Product from './Product';
+import ProductDetail from './ProductDetail';
+import Login from './Login';
+import Cart from './Cart';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <div><Link to='/login'>登入</Link></div>
+      <nav className='leftside-nav'>
+        <Link to='/'>Home</Link>
+        {/*<Link to='/cal'>計算機</Link>*/}
+        <Link to='/product'>產品</Link>
+        <Link to='/cart'>購物車</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/*<Route path='/cal' element={<Cal />} />*/}
+        <Route path='product' element={<Product />} />
+        <Route path='product/:id' element={<ProductDetail />} />
+        <Route path='login' element={<Login />} />
+        <Route path='cart' element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Product.js
@@ -0,0 +1,27 @@
+import { Link, Route, Routes } from "react-router-dom";
+import ProductDetail from "./ProductDetail";
+
+const product_list = [
+    {id: 1, name: '小吉', link:'/00001'},
+    {id: 2, name: '小八貓', link:'/00002'},
+    {id: 3, name: '烏薩奇', link:'/00003'}
+]
+function Product(){
+
+    return(
+        <div>
+            <ul style={{display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '1em'}}>
+                {product_list.map((product, index) => (
+                    <li key={index} style={{display: 'flex', flexDirection: 'column'}}>
+                        <Link to={'/product/' + product.id} style={{display: 'flex', flexDirection: 'column'}} >
+                            <img src={'/img' + product.link + '.png'} style={{width: '10em'}}/>
+                            <label>{product.name}</label>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Product;
